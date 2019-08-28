0995072adbe42810854d4888ac675b9a22bae91b
yabetse <yabetse065@gmail.com>
2019-08-28 17:53:18 +0300

    Corrected typo on File header description

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
  * @summary Translates Amharic pronunciations written in English letters to a set of
  * Amharic letter represenations.
  *
- * @description PhonemeTranslator uses simple phoneme extraction and substituiton to
- * translate Amharic word pronunciations written in Englih to the actual amharic text
- * represenation. It divides the text into sounds that distinguish word meanings
- * based on the seven vowel phonemes in Amharic. Futhermore, Amharic has a rich consonant system.
+ * @description PhonemeTranslator uses simple phoneme extraction and substitution to
+ * translate Amharic word pronunciations written in English to the actual Amharic text
+ * representation. It divides the text into sounds that distinguish word meanings
+ * based on the seven vowel phonemes in Amharic. Furthermore, Amharic has a rich consonant system.
  * A distinguishing feature of consonants in Amharic is the presence of emphatic sounds which
  * are produced with the root of the tongue retracted. All sounds are represented either with
  * a set of english consonant letters or a combination of consonants and a vowel.
@@ -16,7 +16,7 @@
  * represent different pronunciations. For example, the word 'Gebeya' is pronounced as [Ge-be-ya]
  * and when these phonemes are converted to Amharic, it's represented as [ገ - በ - ያ]. Similarly the
  * word Gete is written as [Ge - te] when it's represented as a set of phonemes. However, in this
- * context, the phoneme 'Ge' is pronounced as ጌ not ገ. For this reason, PhonemeTranslator proivdes
+ * context, the phoneme 'Ge' is pronounced as ጌ not ገ. For this reason, PhonemeTranslator provides
  * an array of possible translations based on how a phoneme might be used. This means, the word
  * 'Gebeya' is translated as [ 'ጌቤያ', 'ጌበያ', 'ገቤያ', 'ገበያ' ].
  *
